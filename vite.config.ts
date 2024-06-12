ed76f6ba8c2ad6924b029c4b8f80c6d574b2382b
Darián López Utra <daroxs95@gmail.com>
2024-06-13 01:41:56 +0200

    Setup lib mode

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,13 @@
 import { defineConfig } from "vite";
+import { resolve } from "path";
 
 export default defineConfig({
   base: "./",
+  build: {
+    lib: {
+      entry: resolve(__dirname, "./src/dominique/index.ts"),
+      name: "dominique",
+      fileName: "dominique"
+    }
+  }
 });
